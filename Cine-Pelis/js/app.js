c10463605d4379adc4c3fcebea2b9058148bfd8b
tiendavirtualbatan <tiendavirtualbatan@gmail.com>
2023-02-26 12:53:43 -0600

    Update

--- Cine-Pelis/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  setTimeout(function() {
-    $('#splash').fadeOut(300, function() {
-      window.location.href = 'views/home/home.html';
-    });
-  }, 2000);
-});
